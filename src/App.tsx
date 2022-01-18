2cca8869758bce8b9fa8e6ed99a3e47837b2d99f
vivit <kenji@vivitjp.com>
2022-01-19 06:48:37 +0900

    useEffect Working

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,7 @@ import FakeTable from './component/fake/FakeTable';
 import AppContext from './component/useContext/AppContext';
 import AppContextMin from './component/useContextMin/AppContextMin';
 import AppRef from './component/useRef/AppRef';
-
-const switcher: { [key: string]: boolean } = {
-  Main: false,
-  Fake: false,
-  Context: false,
-  ContextMin: false,
-  Ref: true,
-}
+import AppEffect from './component/useEffect/AppEffect';
 
 function App() {
   return (
@@ -20,11 +13,12 @@ function App() {
       <div className="App-header">REST & fetch: HEADER</div>
       <div className="App-body">
         <div className="App-body-scroll">
-          {switcher.Main && <AppBody />}
-          {switcher.Fake && <FakeTable />}
-          {switcher.Context && <AppContext />}
-          {switcher.ContextMin && <AppContextMin />}
-          {switcher.Ref && <AppRef />}
+          {false && <AppBody />}
+          {false && <FakeTable />}
+          {false && <AppContext />}
+          {false && <AppContextMin />}
+          {false && <AppRef />}
+          {true && <AppEffect />}
         </div>
       </div>
       <div className="App-footer">REST & fetch: FOOTER</div>
